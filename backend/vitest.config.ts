79f0a3645e29672048481e7f198efd18fdf81e44
owen0621 <owen10270936@gmail.com>
2025-05-25 19:51:05 +0800

    fix: vitest

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    pool: 'forks'
+    coverage: {
+      reporter: ['text', 'json-summary', 'html'],
+      reportsDirectory: 'coverage',
+    }
   }
 })
